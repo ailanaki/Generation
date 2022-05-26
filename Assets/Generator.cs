a1b58ee72dda3439830656d7616e1f7501b35c01
aishayakupova <ailanaki@yandex.ru>
2022-05-26 11:59:45 +0300

    split meshes into objects, hardcode scale

--- Assets/Generator.cs
+++ Assets/Generator.cs
@@ -4,29 +4,20 @@ using System.IO;
 using System.Linq;
 using UnityEngine;
 using s1;
-using Mesh = UnityEngine.Mesh;
 
 
 public class Generator : MonoBehaviour
 {
-    
-    Mesh mesh;
-    private List<Vector3> _vertice;
-    private List<int> _triangles;
-    static  List<Vector2> _uv;
-    static double min_x = Double.MaxValue, max_x = Double.MinValue;
-    static double min_y = Double.MaxValue, max_y = Double.MinValue;
-    static double min_z = Double.MaxValue, max_z = Double.MinValue;
+    private double min_x = Double.MaxValue, max_x = Double.MinValue;
+    private double min_y = Double.MaxValue, max_y = Double.MinValue;
+    private double min_z = Double.MaxValue, max_z = Double.MinValue;
+    private int SCALE = 10000;
+
     // Start is called before the first frame update
     void Start()
     {
-        mesh = new Mesh();
-        GetComponent<MeshFilter>().mesh = mesh;
-        MakeMesh();
-        mesh.Clear();
-        mesh.vertices = _vertice.ToArray();
-        mesh.triangles = _triangles.ToArray();
-        mesh.uv = _uv.ToArray();
+        GenerateMeshes();
+
     }
 
     // Update is called once per frame
@@ -34,62 +25,52 @@ public class Generator : MonoBehaviour
     {
     }
 
-     void MakeMesh()
+    void GenerateMeshes()
     {
         var dataList = createDataList(
             "/Users/aishayakupova/RiderProjects/s1/raw/");
-
-        var sizeT = 0;
-         _vertice = new List<Vector3>();
-         _triangles = new List<int>();
-         _uv = new List<Vector2>();
-         var ver = new List<Data.Vertice>();
-         foreach (var data in dataList)
+        int i = 0;
+        foreach (var data in dataList)
         {
-            var meshList = data.Meshes;
-            foreach (var curMesh in meshList)
+            Mesh mesh = new Mesh();
+            var _vertices = new List<Vector3>();
+            var _indices = new List<int>();
+            int count = 0;
+            foreach (var curMesh in data.Meshes)
             {
-                AddCurrentVertice(curMesh, ref ver);
-                AddCurrentIndices(sizeT, curMesh, ref _triangles);
-                sizeT += curMesh.Vertices.Count;
+                addVertices(ref _vertices, curMesh);
+                addIndices(ref _indices, curMesh, count);
+                count += curMesh.Vertices.Count;
             }
+
+            mesh.vertices = _vertices.ToArray();
+            mesh.triangles = _indices.ToArray();
+            var gameObject = new GameObject();
+            gameObject.name = "mesh" + i++;
+            var filter = gameObject.AddComponent<MeshFilter>() as MeshFilter;
+            var rend = gameObject.AddComponent<MeshRenderer>() as MeshRenderer;
+            filter.mesh = mesh;
         }
-        _vertice = Center_Scale(ref ver);
     }
-    
-     List<Data> createDataList(string prefix)
+
+    List<Data> createDataList(string prefix)
     {
         var lines = File.ReadAllLines(prefix + "FileNames.txt");
         return (from line in lines where !String.IsNullOrEmpty(line) select new Data(prefix + line)).ToList();
     }
 
-     static void AddCurrentVertice(Data.CurrentMesh currentMesh, ref List<Data.Vertice> ver)
-     {
-         foreach (var vertex in currentMesh.Vertices)
-         {
-             var _x = vertex.X;
-             var _y =  vertex.Y;
-             var _z = vertex.Z;
-             ver.Add(new Data.Vertice(_x, _y, _z, vertex.U, vertex.V));
-             _uv.Add(new Vector2((float) vertex.U, (float)vertex.V));
-             min_x = Math.Min(_x, min_x);         
-             min_y = Math.Min(_y, min_y);
-             min_z = Math.Min(_z, min_z);
-             max_x = Math.Max(_x, max_x);
-             max_y = Math.Max(_y, max_y);
-             max_z = Math.Max(_z, max_z);
-         }
-     }
-
-    void AddCurrentIndices(int add, Data.CurrentMesh currentMesh, ref List<int> _triangles)
+    void addVertices(ref List<Vector3> ans, Data.CurrentMesh curMesh)
     {
-        foreach (var inx in currentMesh.Indices)
-        {
-            _triangles.Add(inx + add);
-        }
+        ans.AddRange(from vertex in curMesh.Vertices let x = vertex.X / SCALE let y = vertex.Y / SCALE let z = vertex.Z / SCALE select new Vector3((float) x, (float) y, (float) z));
     }
-    
-    static List<Vector3> Center_Scale(ref List<Data.Vertice> ver)
+
+    void addIndices(ref List<int> ans, Data.CurrentMesh curMesh, int count)
+    {
+        ans.AddRange(curMesh.Indices.Select(i => i + count));
+    }
+
+
+    List<Vector3> Center_Scale(ref List<Data.Vertice> ver)
     {
         var center_x = (max_x + min_x) / 2;
         var center_y = (max_y + min_y) / 2;
@@ -105,10 +86,9 @@ public class Generator : MonoBehaviour
             var x = (vertex.X - center_x) / max_distance * SCALE;
             var y = (vertex.Y - center_y) / max_distance * SCALE;
             var z = (vertex.Z - center_z) / max_distance * SCALE;
-            ans.Add(new Vector3((float) x, (float)y, (float) z));
+            ans.Add(new Vector3((float) x, (float) y, (float) z));
         }
 
         return ans;
     }
-    
 }
